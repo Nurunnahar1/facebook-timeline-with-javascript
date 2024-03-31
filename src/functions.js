2842fd0da73bdda902a505f97c6caf0edf1ec3cb
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-31 15:49:28 +0600

    data store

--- src/functions.js
+++ src/functions.js
@@ -26,33 +26,6 @@ const createID = () => {
   return timestamp + machineId + processId + counter;
 };
 
-const timeAgo = (timestamp) => {
-  const seconds = Math.floor((new Date() - timestamp) / 1000);
-
-  let interval = Math.floor(seconds / 31536000);
-
-  if (interval > 1) {
-    return interval + " years ago";
-  }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + " months ago";
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + " days ago";
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + " hours ago";
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + " minutes ago";
-  }
-  return Math.floor(seconds) + " seconds ago";
-};
-
 const timeNow = (postDate) => {
   const currentDate = new Date();
   const diff = currentDate - postDate;
@@ -80,3 +53,30 @@ const timeNow = (postDate) => {
     return "Just now";
   }
 };
+const timeAgo = (timestamp) => {
+  const seconds = Math.floor((new Date() - timestamp) / 1000);
+
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + " years ago";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + " months ago";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + " days ago";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + " hours ago";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + " minutes ago";
+  }
+  return Math.floor(seconds) + " seconds ago";
+};
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,48 @@
 const post_form = document.getElementById("post-create-form");
+const post_area = document.querySelector(".post-list");
+const post_create_btn = document.getElementById("post-create-btn");
 const msg = document.querySelector(".msg");
+
+const getAllPosts = () => {
+  const posts = JSON.parse(localStorage.getItem("posts"));
+
+  //post loop
+  if (posts) {
+    let posts_data = "";
+
+    posts.reverse().forEach((item, index) => {
+      posts_data += `
+   <div class="post-head">
+        <div class="post-head-up d-flex justify-content-between align-items-center mb-3">
+            <div class="post-profile d-flex align-items-center">
+                <img src="${item.photo}" alt="" class="post-profile-photo me-3">
+                <span class="post-photo-text">${item.name}</span>
+                <div class="upload-time ms-3">
+                    <span>${timeAgo(item.created_at)}</span>
+                    <i class="feather-globe"></i>
+                </div>
+            </div>
+            <div class="post-menu gap-1">
+                <button><i class="fa-solid fa-pen-to-square"></i></button>
+                <button onclick="daletePost('${item.id}')"><i class="fa-solid fa-delete-left"></i></button>
+            </div>
+        </div>
+        <div class="post-caption mb-3">
+            <span class="content">${item.content}</span>
+        </div>
+    </div>
+    <div class="post-upload my-2px">
+        <img src="${item.post_photo}" alt="" class="post-upload-img img-fluid">
+    </div>
+
+
+               
+            `;
+    });
+    post_area.innerHTML = posts_data;
+  }
+};
+getAllPosts();
 
 post_form.onsubmit = (event) => {
   event.preventDefault();
@@ -20,14 +63,17 @@ post_form.onsubmit = (event) => {
     if (oldData) {
       isData = JSON.parse(oldData);
     }
-      isData.push({
-          id: createID(),
-          name,
-          photo,
-          content,
-          post_photo,
-          crated_at: Date.now()
-      });
-      localStorage.setItem("posts",JSON.stringify(isData));
+    isData.push({
+      id: createID(),
+      name,
+      photo,
+      content,
+      post_photo,
+      created_at: Date.now(),
+    });
+    localStorage.setItem("posts", JSON.stringify(isData));
   }
+  event.target.reset();
+  post_create_btn.click();
+  getAllPosts();
 };
